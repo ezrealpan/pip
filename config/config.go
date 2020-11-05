d9ec0216f203576715f9b3be497c9df892a408f3
panp@next-stage.com.cn <panp@next-stage.com.cn>
2020-11-05 17:14:47 +0800

    【数据采集系统】

--- config/config.go
+++ config/config.go
@@ -140,33 +140,22 @@ func (c *Config) LoadConfigData(data []byte) error {
 		if !ok {
 			return fmt.Errorf("invalid configuration, error parsing field %q as table", name)
 		}
-		fmt.Println("name", name)
-		fmt.Printf("subTable%+v", subTable)
 		switch name {
 		case "inputs", "plugins":
 			for pluginName, pluginVal := range subTable.Fields {
 				switch pluginSubTable := pluginVal.(type) {
 				// legacy [inputs.cpu] support
 				case *ast.Table:
-					fmt.Println("pluginName1", pluginName)
-					fmt.Printf("pluginVal1%+v", pluginVal)
 					if err = c.addInput(pluginName, pluginSubTable); err != nil {
 						return fmt.Errorf("Error parsing %s, %s", pluginName, err)
 					}
 				case []*ast.Table:
-					fmt.Println("pluginName2", pluginName)
-					fmt.Printf("pluginVal2%+v", pluginVal)
-					fmt.Println("----------------------")
 					for _, t := range pluginSubTable {
-						fmt.Printf("pluginVal2-t%+v", t)
-						fmt.Println("----------------------")
 						if err = c.addInput(pluginName, t); err != nil {
 							return fmt.Errorf("Error parsing %s, %s", pluginName, err)
 						}
 					}
 				default:
-					fmt.Println("pluginName3", pluginName)
-					fmt.Printf("pluginVal3%+v", pluginVal)
 					return fmt.Errorf("Unsupported config format: %s",
 						pluginName)
 				}
